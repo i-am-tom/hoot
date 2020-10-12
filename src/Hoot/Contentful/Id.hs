8f1d2bce8753af04b573f05756d4f2ab13029ecc
Tom Harding <tomjharding@live.co.uk>
2020-10-12 20:39:48 +0100

    :fire: Drop `Catalogue` and `Identified`
    Neither of these types are actually that useful, and direct access to
    the `HashMap` saves a lot of boilerplate. `Catalogue` is now just a
    humble type synonym called `Rolodex`.

--- src/Hoot/Contentful/Id.hs
+++ src/Hoot/Contentful/Id.hs
@@ -1,12 +1,8 @@
 {-# LANGUAGE BlockArguments #-}
-{-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE GeneralisedNewtypeDeriving #-}
-{-# LANGUAGE InstanceSigs #-}
-{-# LANGUAGE KindSignatures #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TypeFamilies #-}
 
 {-|
 Module      : Hoot.Contentful.Id
@@ -18,15 +14,14 @@ Stability   : experimental
 -}
 module Hoot.Contentful.Id where
 
-import Data.Aeson ((.:), FromJSON (..), Value)
-import qualified Data.Aeson as Aeson
-import Data.Aeson.Types (Parser)
+import Data.Aeson (FromJSON (..))
 import Data.Hashable (Hashable)
 import Data.HashMap.Strict (HashMap)
 import qualified Data.HashMap.Strict as HashMap
 import Data.Kind (Type)
 import Data.String (IsString)
 import Data.Text (Text)
+import qualified Data.Text as Text
 import GHC.Generics (Generic)
 import Prelude hiding (id)
 
@@ -35,46 +30,18 @@ import Prelude hiding (id)
 -- that they can't be mixed up.
 newtype Id (entity :: Type)
   = Id { toText :: Text }
-  deriving stock (Eq, Generic, Ord, Show)
+  deriving stock (Eq, Generic, Ord)
   deriving newtype (FromJSON, Hashable, IsString)
 
--- | An /identified/ entity is an entity that has an identifier. Specifically,
--- we expect the identifier to be in the entity's JSON under @.sys.id@.
-data Identified (entity :: Type)
-  = Identified
-      { id     :: Id entity
-      , entity :: entity
-      }
-  deriving stock (Eq, Generic, Ord, Show)
-  deriving anyclass (Hashable)
+instance Show (Id x) where
+  show = show . Text.unpack . toText
 
--- | Convert an 'Identified' entity into a pair of its identifier and the
--- entity itself. This mostly exists for internal use in 'parseJSON', and
--- probably isn't useful anywhere else.
-toPair :: Identified x -> (Id x, x)
-toPair Identified{..} = ( id, entity )
+-- | A Rolodex is a 'HashMap' of entities indexed by an identifier. Naming
+-- things is the hardest problem in computer science.
+type Rolodex (x :: Type)
+  = HashMap (Id x) x
 
-instance FromJSON x => FromJSON (Identified x) where
-  parseJSON :: Value -> Parser (Identified x)
-  parseJSON json = do
-    let parseIdentifier :: Value -> Parser (Id entity)
-        parseIdentifier = Aeson.withObject "Entity" \asset ->
-          asset .: "sys" >>= Aeson.withObject "sys" \sys ->
-            sys .: "id"
-
-    id     <- parseIdentifier json
-    entity <- parseJSON json
-
-    pure Identified{..}
-
--- | An entity catalogue is a 'HashMap' in which the keys are 'Id' references
--- to their entity values. For our purposes, entity catalogues are likely to be
--- filled with @Entry@, @Asset@, and @ContentType@ values.
-newtype Catalogue (entity :: Type)
-  = Catalogue { toHashMap :: HashMap (Id entity) entity }
-  deriving stock (Eq, Generic, Ord, Show)
-  deriving anyclass (Hashable)
-
-instance FromJSON x => FromJSON (Catalogue x) where
-  parseJSON :: Value -> Parser (Catalogue x)
-  parseJSON = fmap (Catalogue . HashMap.fromList . map toPair) . parseJSON
+-- | Given a way to extract an identifier, turn a list of items into a map of
+-- values indexed by that identifier.
+index :: (x -> Id x) -> [x] -> HashMap (Id x) x
+index f = HashMap.fromList . map \x -> (f x, x)
